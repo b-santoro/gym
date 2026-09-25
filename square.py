571e5503f5c57696e5362ba33e3d78a1854a9eec
b-santoro <bennysantoro01@gmail.com>
2026-09-25 15:43:21 +0200

    Aggiunto lo script square.py

---
+++ square.py
@@ -0,0 +1,9 @@
+def square (x):
+    return x*x
+# Chiede un numero all'utente nel terminale
+numero = float(input("Inserisci un numero: "))
+
+# Esegue la funzione e mostra il risultato
+risultato = square(numero)
+print(f"Il risultato è: {risultato}")
+    
